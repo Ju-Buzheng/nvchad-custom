1a4d097a7575334080310280ff422bc66b3f7b73
Juhan Zhang <juhan.zhang@hannto.com>
2023-05-05 23:53:18 +0800

    feat(configs): First commit for my configs

---
+++ init.lua
@@ -0,0 +1,28 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+--
+-- Powershell setting
+local isWindows = function () 
+  local arch = os.getenv("os")
+  if string.match(arch, "Windows") then
+    return true
+  end
+  return false
+end
+
+if isWindows() then
+  local cmd = "pwsh.exe"
+  local avaiable = vim.fn.executable(cmd) 
+  vim.o.shell = avaiable and cmd or "powershell.exe"
+  vim.o.shellcmdflag =  "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
+  vim.o.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+  vim.o.shellpipe = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
+  vim.o.shellquote = nil 
+  vim.o.shellxquote = nil
+end
+
