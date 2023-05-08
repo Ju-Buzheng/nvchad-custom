85e7c724e8fb1a93859af093f121d3e80983c964
Juhan Zhang <juhan.zhang@hannto.com>
2023-05-08 10:04:01 +0800

    fix(INIT): just set tabsize to 2 for lua buffer

--- init.lua
+++ init.lua
@@ -35,8 +35,8 @@ vim.opt.softtabstop = 4
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "lua",
   callback = function ()
-    vim.opt.shiftwidth = 2
-    vim.opt.tabstop = 2
-    vim.opt.softtabstop = 2
+    vim.bo.shiftwidth = 2
+    vim.bo.tabstop = 2
+    vim.bo.softtabstop = 2
   end,
 })
