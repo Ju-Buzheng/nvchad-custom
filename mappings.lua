e70c507983aa98c871b7d5ee3cee5230db82b7f5
Juhan <juhan.zhang@hannto.com>
2023-05-10 21:37:16 +0800

    feat(INIT): add rust setup

--- mappings.lua
+++ mappings.lua
@@ -8,5 +8,30 @@ M.general = {
 }
 
 -- more keybinds!
+M.dap = {
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Toggle breakpoint"
+    },
+    ["<leader>dus"] = {
+      function ()
+        local dapui = require("dapui")
+        dapui.toggle()
+      end,
+      "Open debugging sidebar"
+    },
+  }
+}
+
+M.crates = {
+  n = {
+    ["<leader>rcu"] = {
+      function ()
+        require("crates").upgrade_all_crates()
+      end
+    }
+  }
+}
 
 return M
